a9679556041a2d105e5dea6231e430c85c064447
Aleksei Bekh-Ivanov <bekh6ex@no-reply.github.com>
2023-06-01 14:37:20 +0200

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ssl-pinning-remover",
-    version="1.0.0",
+    version="1.0.1",
     author="HexNio",
     author_email="",
     description="An SSL Pinning Remover for Android Apps",
